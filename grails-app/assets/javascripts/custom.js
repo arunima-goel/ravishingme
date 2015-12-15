8a761b0d47a5a1d71e4e9e7fde9f5e24865d9c94
arunima-goel <goel.arunima@gmail.com>
2015-12-15 00:27:38 -0600

    index page

---
+++ grails-app/assets/javascripts/custom.js
@@ -0,0 +1,323 @@
+$(document).ready(function() {
+    
+    /* Add/Remove Active Class */
+    $("ul.second-nav li").click(function() {  
+        $(this).addClass('selected').siblings().removeClass('selected');
+    });
+
+    /* Smooth Scrolling Effect */
+    $(document).on('click', '.second-nav a', function(e) {
+       e.preventDefault();
+       $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top}, 500);
+    })
+
+});
+
+/* Custom Checkboxes & Radio Buttons */
+$('.settings-input input[type="checkbox"]').each(function(){
+    $(this).prettyCheckable();
+ });
+
+$('.settings-input input[type="radio"]').each(function(){
+    $(this).prettyCheckable();
+ });
+
+$(".search-header-checkboxes input[type='checkbox']").each(function(){
+    $(this).prettyCheckable();
+});
+
+$(".filter-header-checkboxes input[type='checkbox']").each(function(){
+    $(this).prettyCheckable();
+});
+
+$('.filter-checkboxes label, .filter-header-checkboxes').click(function(e) {
+     e.stopPropagation();
+});
+
+$('.search-header-checkboxes label, .search-header-checkboxes').click(function(e) {
+    e.stopPropagation();
+    
+    var checkedValues = $('.search-header-checkboxes input:checkbox:checked').map(function() {
+        return this.value;
+    }).get();
+    
+    
+    console.log("Checked: " + checkedValues.join(', '));
+    
+    $(".sh-search .dropdown-search-btn, .home-content .dropdown-search-btn").text(checkedValues.join(', '));
+});
+
+
+$('.travel-filter ul.dropdown-menu li a').click(function(e) {
+    e.stopPropagation();
+    
+    // If this isn't already active
+    if (!$(this).hasClass("active")) {
+        // Remove the class from anything that is active
+        $(".travel-filter ul.dropdown-menu li a.active").removeClass("active");
+        // And make this active
+        $(this).addClass("active");
+    }
+});
+
+
+/* Change Dropdown Buttons Text */
+$(function(){
+      $(".home-city .dropdown-menu li, .sh-city .dropdown-menu li").click(function(){
+          $(".dropdown-city-btn").text($(this).text());
+          $(".dropdown-city-btn").val($(this).text());
+          $(".home-city .dropdown-menu li a, .sh-city .dropdown-menu li a").removeClass("selected");
+          $(this).addClass("selected");
+      });
+});
+
+$(function(){
+      $(".home-city .dropdown-menu li").click(function(){
+          $(".dropdown-city-btn").text($(this).text());
+          $(".dropdown-city-btn").val($(this).text());
+          $(".home-city .dropdown-menu li").removeClass("selected");
+          $(this).addClass("selected");
+      });
+});
+
+
+$(function(){
+      $(".settings-dropdown-city li").click(function(){
+          $(".settings-dropdown-btn.city").text($(this).text());
+          $(".settings-dropdown-btn.city").val($(this).text());
+          $(".settings-dropdown-city li").removeClass("selected");
+          $(this).addClass("selected");
+      });
+});
+
+$(function(){
+      $(".settings-dropdown-state li").click(function(){
+          $(".settings-dropdown-btn.state").text($(this).text());
+          $(".settings-dropdown-btn.state").val($(this).text());
+          $(".settings-dropdown-state li").removeClass("selected");
+          $(this).addClass("selected");
+      });
+});
+
+$(function(){
+      $(".dropdown-menu.outer-noscroll.settings-dropdown-hours-am li").click(function(){
+          $(".settings-dropdown-btn.am").text($(this).text());
+          $(".settings-dropdown-btn.am").val($(this).text());
+          $(".dropdown-menu.outer-noscroll.settings-dropdown-hours-am li").removeClass("selected");
+          $(this).addClass("selected");
+      });
+});
+
+$(function(){
+      $(".dropdown-menu.outer-noscroll.settings-dropdown-hours-pm li").click(function(){
+          $(".settings-dropdown-btn.pm").text($(this).text());
+          $(".settings-dropdown-btn.pm").val($(this).text());
+          $(".dropdown-menu.outer-noscroll.settings-dropdown-hours-pm li").removeClass("selected");
+          $(this).addClass("selected");
+      });
+});
+
+$(function(){
+      $(".dropdown-menu.outer-noscroll.settings-dropdown-menu.hours-am1 li").click(function(){
+          $(".settings-dropdown-btn.hours-am").text($(this).text());
+          $(".settings-dropdown-btn.hours-am").val($(this).text());
+          $(".dropdown-menu.outer-noscroll.settings-dropdown-menu.hours-am1 li").removeClass("selected");
+          $(this).addClass("selected");
+      });
+});
+
+$(function(){
+      $(".dropdown-menu.outer-noscroll.settings-dropdown-menu.hours-am li").click(function(){
+          $(".settings-dropdown-btn.hours-pm").text($(this).text());
+          $(".settings-dropdown-btn.hours-pm").val($(this).text());
+          $(".dropdown-menu.outer-noscroll.settings-dropdown-menu.hours-am li").removeClass("selected");
+          $(this).addClass("selected");
+      });
+});
+
+$(function(){
+      $(".sh-city .dropdown-menu.outer-noscroll li").click(function(){
+          $(".dropdown-city-btn").text($(this).text());
+          $(".dropdown-city-btn").val($(this).text());
+          $(".sh-city .dropdown-menu.outer-noscroll li").removeClass("selected");
+          $(this).addClass("selected");
+      });
+});
+
+
+$('.cosmetics-filter .dropdown-scroll').on('show.bs.dropdown', function () {
+    $(".cosmetics-filter a.filter-search-btn.fixed-btn").show();
+});
+
+$('.cosmetics-filter .dropdown-scroll').on('hide.bs.dropdown', function () {
+    $(".cosmetics-filter a.filter-search-btn.fixed-btn").hide();
+});
+
+$('.makeup-filter .dropdown-scroll').on('show.bs.dropdown', function () {
+    $(".makeup-filter a.filter-search-btn.fixed-btn").show();
+});
+
+$('.makeup-filter .dropdown-scroll').on('hide.bs.dropdown', function () {
+    $(".makeup-filter a.filter-search-btn.fixed-btn").hide();
+});
+
+
+/* Settings Menu */
+$("input[name='artist']").change(function(){
+    if($(this).val() == 'Yes'){
+        $(".artist-menu-item").show();
+        $(".user-menu-item").hide();
+        $(".whatsapp-msg").show();
+    } else{
+        $(".artist-menu-item").hide();
+        $(".user-menu-item").show();
+        $(".whatsapp-msg").hide();
+    }
+});
+
+/* Change Heart Image on Favorites Settings */
+$('img.fav-heart').on({
+    'click': function(){
+        var src = ($(this).attr('src') === 'img/heart-orange.png')
+            ? 'img/heart-gray.png'
+            : 'img/heart-orange.png';
+         $(this).attr('src', src);
+    }
+});
+
+$('img.heart-img').on({
+    'click': function(){
+        var src = ($(this).attr('src') === 'img/heart-orange.png')
+            ? 'img/heart-gray.png'
+            : 'img/heart-orange.png';
+         $(this).attr('src', src);
+    }
+});
+
+
+/* Custom Form Validation */
+$(".save-btn").click(function(){
+    $(".settings-form").validator('validate')
+});
+
+
+$("ul.dropdown-menu.outer-noscroll.settings-dropdown-city li").click(function() {
+    var value = $(this).text();
+    
+    var isValid = (value !== 'City')
+    
+    if (isValid) {
+        $(".city-dropdown .help-block.with-errors").text("");
+    }
+    
+});  
+
+$("ul.dropdown-menu.outer-noscroll.settings-dropdown-state li").click(function() {
+    var value = $(this).text();
+    
+    var isValid = (value !== 'State')
+    
+    if (isValid) {
+        $(".state-dropdown .help-block.with-errors").text("");
+    }
+    
+}); 
+
+$(".save-btn").click(function(event) {
+    
+    /** Location Validation **/
+    var value = $(".settings-dropdown-btn.city").text();
+    var isValid = (value !== 'City')
+    
+    if (!isValid) {
+        $(".city-dropdown .help-block.with-errors").text("Please select your city");
+        event.preventDefault();
+    } else {
+        $(".city-dropdown .help-block.with-errors").text("");
+    }
+    
+    var value = $(".settings-dropdown-btn.state").text();
+    var isValid = (value !== 'State')
+    
+    if (!isValid) {
+        $(".state-dropdown .help-block.with-errors").text("Please select your state");
+        event.preventDefault();
+    } else {
+        $(".state-dropdown .help-block.with-errors").text("");
+    }
+    
+    /** Radio Button Validation **/
+    if ($('#artist-yes').is(':checked') || $('#artist-no').is(':checked')) {  
+        $(".artist-container .help-block.with-errors").text("");
+    }else{
+        $(".has-radio .help-block.with-errors").text("Please make a selection.");
+        event.preventDefault();
+    }
+    
+    /** Profile Validation **/
+    if ($('#travel-yes').is(':checked') || $('#travel-no').is(':checked')) {  
+        $(".travel-container .help-block.with-errors").text("");
+    }else{
+        $(".travel-container .help-block.with-errors").text("Please make a selection.");
+        event.preventDefault();
+    }
+    
+    checkChecked('.settings-form');
+    checkCheckeda('.settings-form');
+    
+    if(!$(".service-checkbox").prop('checked')){
+      $(".services.help-block.with-errors").text("Please select at least one.");
+      event.preventDefault();
+    }
+
+});
+
+/* Clear Radio Error */
+$(".clearfix.prettyradio.labelright.blue").click(function(){
+    $(".has-radio .help-block.with-errors").text("");
+});
+
+$(".clearfix.prettycheckbox.labelright.cosmetics-check.blue").click(function(){
+        $(".cosmetics.help-block.with-errors").text("");
+});
+
+$(".clearfix.prettycheckbox.labelright.services-check.blue").click(function(){
+    $(".services.help-block.with-errors").text("");
+});
+
+
+/* Check if checkboxes are checked */
+function checkChecked(formname) {
+    var anyBoxesChecked = false;
+    $('.cos-checkbox').each(function() {
+        if ($(this).prop('checked')) {
+            anyBoxesChecked = true;
+        }
+    });
+ 
+    if (anyBoxesChecked == false) {
+      $(".cosmetics.help-block.with-errors").text("Please select at least one.");
+      event.preventDefault();  
+    } 
+}
+
+/* Check if checkboxes are checked */
+function checkCheckeda(formname) {
+    var anyBoxesChecked = false;
+    $('.service-checkbox').each(function() {
+        if ($(this).prop('checked')) {
+            anyBoxesChecked = true;
+        }
+    });
+ 
+    if (anyBoxesChecked == false) {
+      $(".services.help-block.with-errors").text("Please select at least one.");
+      event.preventDefault();  
+    } 
+}
+
+
+
+
+
--- grails-app/views/index.gsp
+++ grails-app/views/index.gsp
@@ -1,23 +1,182 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-    <title>Welcome to Grails</title>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>ravishing.me</title>
+
+    <!-- Bootstrap Core CSS -->
+    <asset:stylesheet src="bootstrap.min.css"/>
+    
+    <!-- Font Awesome CSS -->
+    <asset:stylesheet href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"/>
+    
+    <!-- Custom Checkboxes -->
+    <asset:stylesheet src="prettyCheckable.css"/>
+    
+    <!-- Custom CSS -->
+    <asset:stylesheet src="style.css"/>
+   
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    
 </head>
 
 <body>
-<oauth:disconnected provider="facebook">
- <span class="vert hidden-xs">&vert;</span>
- <li>
-     <oauth:connect provider="facebook" id="facebook-connect-link" class="facebookLogin">login</oauth:connect>
- </li>
- <span class="vert hidden-xs">&vert;</span>
- <li>
-     <oauth:connect provider="facebook" id="facebook-connect-link" class="facebookLogin">sign up</oauth:connect>
- </li>
-</oauth:disconnected>
-<oauth:connected provider="facebook">
-<span class="vert hidden-xs">&vert;</span>
-<li>
-    <g:link controller="user" action="logout" id="facebook">Logout</g:link>
-</li>
- </oauth:connected>
+
+    <!--Header_section-->
+    <header id="header-wrapper">
+        
+      <div class="container-fluid">
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <div class="container-fluid nav-container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <a href="index.html" class="navbar-brand"><asset:image class="logo" src="logo.png" alt="Ravishing.me" /></a>
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse-container">
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li>
+                                <a href="about.html">About</a>
+                            </li>
+                            <li>
+                                <a href="about.html">FAQS</a>
+                            </li>
+                            <li>
+                                <a href="#">Sign in</a>
+                            </li>
+                            <li>
+                                <a class="signup-highlight" href="#" data-toggle="modal" data-target="#signup">Sign up</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container -->
+          </nav>
+      </div><!--- Header Container -->
+    </header>
+    <!--Header_section--> 
+
+    <!-- Page Content -->
+    <div id="home-wrapper">
+        <asset:image class="home-bg" alt="home" src="home_bg.png" />
+        <div class="container home-content">
+            <div class="row">
+                <div class="col-md-5 col-md-offset-1 col-xs-12 home-search">
+                    <span class="drp-caret"></span>
+                    <div class="button-group">
+                        <button type="button" class="dropdown-search-btn" data-toggle="dropdown">Bridal Makeup</button>
+                        <ul class="search-header-checkboxes dropdown-menu outer-noscroll">
+                            <div class="inner-noscroll">
+                                <li><input type="checkbox" name="cosmetics" value="Bridal Makeup" data-label="Bridal Makeup" checked/></li>
+                                <li><input type="checkbox" name="cosmetics" value="Engagement" data-label="Engagement"/></li>
+                                <li><input type="checkbox" name="cosmetics" value="Eye Makeup" data-label="Eye Makeup"/></li>
+                                <li><input type="checkbox" name="cosmetics" value="Fashion Shows" data-label="Fashion Shows"/></li>
+                                <li><input type="checkbox" name="cosmetics" value="Hairstyle" data-label="Hairstyle"/></li>
+                                <li><input type="checkbox" name="cosmetics" value="Light Makeup" data-label="Light Makeup"/></li>
+                                <li><input type="checkbox" name="cosmetics" value="Engagement" data-label="Engagement"/></li>
+                                <li><input type="checkbox" name="cosmetics" value="Eye Makeup" data-label="Eye Makeup"/></li>
+                            </div>
+                            <div class="scroll-opac"></div>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-5 col-xs-12 home-city">
+                    <div class="input-group">
+                        <div class="drp-caret"></div>
+                        <div class="button-group">
+                        <button type="button" class="dropdown-city-btn" data-toggle="dropdown">Delhi</button>
+                            <ul class="dropdown-menu outer-noscroll">
+                                <div class="inner-noscroll">
+                                    <li><a>Ahemdabad</a></li>
+                                    <li><a>Chennai</a></li>
+                                    <li class="selected"><a>Delhi</a></li>
+                                    <li><a>Goa</a></li>
+                                    <li><a>Hyderabad</a></li>
+                                    <li><a>Chennai</a></li>
+                                    <li><a>Ahemdabad</a></li>
+                                    <li><a>Chennai</a></li>
+                                </div>
+                                <div class="scroll-opac"></div>
+                            </ul>
+                        </div>
+                        <span class="input-group-btn">
+                            <button class="btn btn-default home-btn" type="button">Search</button>
+                        </span>
+                    </div><!-- /input-group -->
+                </div><!-- /home-city row -->
+            </div><!-- /Search Bar Row -->
+        </div><!-- Home Content Container -->
+    </div><!-- /Home Wrapper -->
+    
+    <!-- Callout -->
+    <div class="home-callout container">
+       <div class="row">
+          <h3>Be Ravishing,
+           <span>Find and contact beauty professionals near you<span></h3>
+            <hr/>
+        </div><!-- /Row -->
+    </div>
+           
+    <!-- Signup Modal -->
+    <div id="signup" class="modal fade" role="dialog">
+      <div class="modal-dialog">
+      <button class="modal-close" data-dismiss="modal"></button>
+        <!-- Modal content-->
+        <div class="modal-content text-center">
+          <div class="modal-header">
+              <h4 class="modal-title">Create your ravishing account.</h4> 
+              <h4>It's <span>Free</span></h4>
+          </div>
+          <div class="modal-body">
+            <a href="#" class="btn btn-block btn-social btn-facebook"><span class="fa fa-5x fa-facebook"></span>Sign up with Facebook</a>
+            <p>We will not post anything without your permission</p>
+          </div>
+          <div class="signup-terms">
+              <p>By clicking "sign up", you confirm that you accept</p><p> our <a href="#">Terms of service</a> and <a href="#">Privacy Policy</a>.</p>  
+          </div>
+          <div class="modal-footer">
+              <h4>Already a member? <a href="#">Login</a></h4>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <!-- jQuery -->
+    <asset:javascript src="jquery.js"/>
+    
+    <!-- Bootstrap Core JavaScript -->
+    <asset:javascript src="bootstrap.min.js"/>
+           
+    <!-- Custom Checkboxes -->
+    <asset:javascript src="prettyCheckable.min.js"/>
+    
+    <!-- Custom JS -->
+    <asset:javascript src="custom.js"/>
+    
 </body>
+
 </html>
