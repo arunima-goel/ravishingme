1c2ac4e90931c43a497b872ccb4b7fa1c5e86a1e
arunima-goel <goel.arunima@gmail.com>
2016-01-11 01:50:11 -0600

    Base controller clean up

--- grails-app/controllers/ravishingme/BaseController.groovy
+++ grails-app/controllers/ravishingme/BaseController.groovy
@@ -1,58 +1,66 @@
 package ravishingme
 
-import grails.converters.JSON
-
-import org.ravishingme.Address;
-import org.ravishingme.BusinessHours;
-import org.ravishingme.City;
-import org.ravishingme.SecRole
-import org.ravishingme.SecUser
-import org.ravishingme.SecUserSecRole
-import org.ravishingme.SocialNetworks;
-import org.scribe.model.Token
+import org.ravishingme.Address
+import org.ravishingme.BusinessHours
+import org.ravishingme.City
 import org.ravishingme.Profile
+import org.ravishingme.SecUser
+import org.ravishingme.SocialNetworks
+import org.scribe.model.Token
 
 class BaseController {
 	def oauthService
 	def userService
 	def facebookService
 
+	/**
+	 * Endpoint to render the about page
+	 */
 	def about() {
-		log.info("Params: " + params);
-		def loggedInUser = getLoggedInUser();
+		log.info("about() - begin - params [" + params + "]");
 		def searchParams = [:];
 		searchParams["city"] = "1";
-		render(view:'/about', model: [profile:buildRavMeProfile(), loggedInUser: loggedInUser, searchParams: searchParams])
+		render(view:'/about', model: [profile:buildRavMeProfile(), loggedInUser: getLoggedInUser(), searchParams: searchParams])
+		log.info("about() - end");
 	}
 	
+	/**
+	 * Endpoint to render the faqs page
+	 */
 	def faqs() {
-		log.info("Params: " + params);
-		def loggedInUser = getLoggedInUser();
+		log.info("faqs() - begin - params [" + params + "]");
 		def searchParams = [:];
-		searchParams["city"] = "1"; 
-		render(view:'/faqs', model: [loggedInUser: loggedInUser, searchParams: searchParams])
+		searchParams["city"] = "1";
+		render(view:'/faqs', model: [loggedInUser: getLoggedInUser(), searchParams: searchParams])
+		log.info("faqs() - end");
 	}
-	
+
+	/**
+	 * Helper method to get logged in user
+	 */
 	def getLoggedInUser() {
-		log.info("Getting logged in user")
-		Token facebookAccessToken = (Token) session[oauthService.findSessionKeyForAccessToken('facebook')]
+		log.info("getLoggedInUser() - begin");
+		SecUser loggedInUser = null;
+		Token facebookAccessToken = (Token) session[oauthService.findSessionKeyForAccessToken('facebook')];
 		try {
 			// Get user id and username from facebook
 			def (userid, name) = facebookService.getUserIdAndName(facebookAccessToken, "me");
-			SecUser loggedInUser = SecUser.findByUserid(userid);
-			Profile loggedInUserProfile = Profile.findById(loggedInUser.profile.id);
-			log.info("Got logged in user userId: " + userid + " name: " + name + " profile username: " + loggedInUser.profile.username)
-			loggedInUser.profile = loggedInUserProfile;
-			return loggedInUser;
+			loggedInUser = userService.findUserByUserId(userid);
 		} catch (CustomException ce) {
-			log.info("Error getting logged in user")
-			flash.error = "Exception during login"
+			log.info("Error getting logged in user: " + ce.getErrorMessage());
 		}
+		log.info("getLoggedInUser() - end - Logged in user [" + loggedInUser + "]");
+		return loggedInUser;
 	}
-	
+
+	/**
+	 * Helper method to build the ravishing me profile for the about page
+	 * @return
+	 */
 	def buildRavMeProfile() {
+		log.info("buildRavMeProfile() - begin");
 		def profile = new Profile("ravishing-me", "Ravishing.me");
-		
+
 		Address address = new Address();
 		City city = City.findByName("Delhi");
 		address.setCity(city);
@@ -61,7 +69,7 @@ class BaseController {
 		address.setStreetAddress("");
 		address.save(failOnError:true);
 		profile.setAddress(address);
-		
+
 		BusinessHours businessHours = new BusinessHours();
 		businessHours.setStartTime(BusinessHours.Time.TEN);
 		businessHours.setStartTimePeriod(BusinessHours.Period.AM);
@@ -76,8 +84,9 @@ class BaseController {
 		socialNetworks.facebookUrl = "www.facebook.com/Ravishingme-1621934231391175";
 		socialNetworks.save(failOnError:true);
 		profile.setSocialNetworks(socialNetworks);
-			
+
 		profile.setYearsOfExperience(0);
+		log.info("buildRavMeProfile() - end");
 		return profile;
 	}
 
