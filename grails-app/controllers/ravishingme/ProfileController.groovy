540fe793f8396ae033fd68275186a4f3e5008e76
arunima-goel <goel.arunima@gmail.com>
2016-01-11 01:46:35 -0600

    Profile controller cleanup

--- grails-app/controllers/ravishingme/ProfileController.groovy
+++ grails-app/controllers/ravishingme/ProfileController.groovy
@@ -1,33 +1,28 @@
 package ravishingme
 
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponse
 
-import grails.converters.JSON
-
-import org.ravishingme.Address;
-import org.ravishingme.BusinessHours;
-import org.ravishingme.City;
-import org.ravishingme.SecRole
-import org.ravishingme.SecUser
-import org.ravishingme.SecUserSecRole
-import org.ravishingme.SocialNetworks;
-import org.scribe.model.Token
 import org.ravishingme.Profile
+import org.ravishingme.SecUser
+import org.scribe.model.Token
 
 class ProfileController {
 
 	def oauthService
 	def facebookService
+	def userService
 
+	/**
+	 * Endpoint to update settings of a profile
+	 */
 	def update() {
-		log.info("Updating profile: " + params);
+		log.info("update() - begin - params [" + params + "]");
 
 		def profileInstance = Profile.get(params.id);
 
 		// ***deselected values don't get saved if we don't clear the values here
 		if (profileInstance.getIsArtist()) {
 			profileInstance.cosmeticBrands.clear();
-			profileInstance.servicesOffered.clear();
 		} else {
 			profileInstance.preferredCosmeticBrands.clear();
 			profileInstance.preferredServices.clear();
@@ -40,13 +35,16 @@ class ProfileController {
 		profileInstance.address.save(flush:true);
 		profileInstance.save(flush:true);
 
-		flash.info = "Successful profile update";
 		render(template:'/admin/profileInfo', model: [profile: Profile.findByUsername(params.username)])
+		log.info("update() - end");
 	}
 
-	def updateSettings() {
+	/**
+	 * Endpoint to update settings
+	 */
+	def updateSettings() { // TODO: check where this is called?
 		// TODO: get logged in user and compare the ID here - do not update if there is no
-		log.info("Updating profile: " + params);
+		log.info("updateSettings() - begin - params [" + params + "]");
 
 		def profileInstance = Profile.get(params.id);
 
@@ -65,90 +63,115 @@ class ProfileController {
 		profileInstance.address.save(flush:true);
 		profileInstance.save(flush:true);
 
-		flash.info = "Successful profile update";
-		render status:HttpServletResponse.SC_NO_CONTENT 
+		render status:HttpServletResponse.SC_NO_CONTENT
+		log.info("updateSettings() - end");
 	}
 
+	/**
+	 * Endpoint to get profile information by username
+	 * @param username Username of the profile that needs to be fetched
+	 * @return The profile with the given username
+	 */
 	def index(String username) {
-		log.info("Getting profile: " + username)
+		log.info("index() - begin - params [" + params + "] - username [" + username + "]");
 		try {
 			def profile = Profile.findByUsername(username)
 			if (profile) {
-				// checkMinContent(username) // if logged in user is the same as the username,
+				// TODO:  checkMinContent(username) // if logged in user is the same as the username,
 				// then check min content and display edit page
-				def loggedInUser = getLoggedInUser();
-				[profile:profile, loggedInUser: loggedInUser]
+				log.info("index() - end");
+				[profile:profile, loggedInUser: getLoggedInUser()]
 			} else {
 				redirect(uri: "/")
+				log.info("index() - end");
 			}
-
 		} catch (Exception e) {
-			flash.error = "Exception during profile index"
+			log.error("There was an error while fetching profile for user " + username + " " + e.getMessage());
+			flash.error = "There was an error while fetching profile for user " + username;
+			redirect(uri: "/")
+			log.info("index() - end");
 		}
-
-
 	}
 
+	/**
+	 * Endpoint to get the settings of the currently logged in user
+	 * @return Settings of the logged in user
+	 */
 	def settings() {
+		log.info("settings() - begin - params [" + params + "]");
 		try {
-			log.info("Settings");
-			// checkMinContent(username) // if logged in user is the same as the username,
+			// TODO: checkMinContent(username) // if logged in user is the same as the username,
 			// then check min content and display edit page
-			def loggedInUser = getLoggedInUser();
-			[loggedInUser: loggedInUser]
+			log.info("settings() - end");
+			[loggedInUser: getLoggedInUser()]
 		} catch (Exception e) {
-			flash.error = "Exception during profile index"
+			flash.error = "There was an error while fetching your settings. Please contact ravishing@ravishingme.com.";
+			redirect(uri: "/")
+			log.info("settings() - end");
 		}
 	}
 
-	def checkMinContent(String name) {
+	/**
+	 * Helper method to check minimum content for a profile
+	 * @param name
+	 * @return
+	 */
+	def checkMinContent() {
 	}
 
+	/**
+	 * Helper method to get logged in user
+	 */
 	def getLoggedInUser() {
-		log.info("Getting logged in user")
-		Token facebookAccessToken = (Token) session[oauthService.findSessionKeyForAccessToken('facebook')]
-		if (facebookAccessToken != null) {
-			try {
-				// Get user id and username from facebook
-				def (userid, name) = facebookService.getUserIdAndName(facebookAccessToken, "me");
-				SecUser loggedInUser = SecUser.findByUserid(userid);
-				Profile loggedInUserProfile = Profile.read(loggedInUser.profile.id);
-				log.info("Got logged in user userId: " + userid + " name: " + name + " profile username: " + loggedInUser.profile.username)
-				loggedInUser.profile = loggedInUserProfile;
-				return loggedInUser;
-			} catch (CustomException ce) {
-				log.info("Error getting logged in user")
-				flash.error = "Exception during login"
-			}
+		log.info("getLoggedInUser() - begin");
+		SecUser loggedInUser = null;
+		Token facebookAccessToken = (Token) session[oauthService.findSessionKeyForAccessToken('facebook')];
+		try {
+			// Get user id and username from facebook
+			def (userid, name) = facebookService.getUserIdAndName(facebookAccessToken, "me");
+			loggedInUser = userService.findUserByUserId(userid);
+		} catch (CustomException ce) {
+			log.info("Error getting logged in user: " + ce.getErrorMessage());
 		}
+		log.info("getLoggedInUser() - end - Logged in user [" + loggedInUser + "]");
+		return loggedInUser;
 	}
 
+	/**
+	 * Endpoint to add a favorite
+	 */
 	def addFavorite() {
-		log.info("Adding favorite")
+		log.info("addFavorite() - begin - params [" + params + "]");
 		def favoriteProfileInstance = Profile.get(params.favoriteId)
 
 		def loggedInUser = getLoggedInUser();
 		def profileInstance = loggedInUser.profile;
 		profileInstance.addToFavorites(favoriteProfileInstance)
 		profileInstance.save(flush: true)
-		render(template:'/profile/favoriteIcon', model: [profile:favoriteProfileInstance, loggedInUser: getLoggedInUser()])
+		render(template:'/profile/favoriteIcon', model: [profile:favoriteProfileInstance, loggedInUser: loggedInUser])
+		log.info("addFavorite() - end");
 	}
 
+	/**
+	 * Endpoint to remove a favorite
+	 */
 	def removeFavorite() {
-		log.info("Removing favorite")
-		log.info("\nFavorite Id: " + params.favoriteId + "\nId: " + params.id)
+		log.info("removeFavorite() - begin - params [" + params + "]");
 		def favoriteProfileInstance = Profile.findById(params.favoriteId)
 
 		def loggedInUser = getLoggedInUser();
 		def profileInstance = loggedInUser.profile;
 		profileInstance.removeFromFavorites(favoriteProfileInstance)
 		profileInstance.save(flush: true)
-		render(template:'/profile/favoriteIcon', model: [profile:favoriteProfileInstance, loggedInUser: getLoggedInUser()])
+		render(template:'/profile/favoriteIcon', model: [profile:favoriteProfileInstance, loggedInUser: loggedInUser])
+		log.info("removeFavorite() - end");
 	}
 
-	def removeFavoriteFromSettings() {
-		log.info("Removing favorite")
-		log.info("\nFavorite Id: " + params.favoriteId + "\nId: " + params.id)
+	/**
+	 * Endpoint to remove a favorite
+	 */
+	def removeFavoriteFromSettings() { //TODO: check exactly where this is called from
+		log.info("removeFavoriteFromSettings() - begin - params [" + params + "]");
 		def favoriteProfileInstance = Profile.findById(params.favoriteId)
 
 		def loggedInUser = getLoggedInUser();
@@ -156,6 +179,6 @@ class ProfileController {
 		profileInstance.removeFromFavorites(favoriteProfileInstance)
 		profileInstance.save(flush: true)
 		render(template:'/profile/favoritesSettings', model: [profile:favoriteProfileInstance, loggedInUser: getLoggedInUser()])
-		//	render(template:'/profile/favorites', model: [loggedInUser: getLoggedInUser()])
+		log.info("removeFavoriteFromSettings() - end");
 	}
 }
