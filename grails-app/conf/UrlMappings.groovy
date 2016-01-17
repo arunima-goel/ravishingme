e957bd58a69624cabd7c0fc5b6f7c7707912ab25
arunima-goel <goel.arunima@gmail.com>
2016-01-16 20:09:12 -0600

    404 on page load - now happens only when starting up application

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -6,21 +6,24 @@ class UrlMappings {
 				// apply constraints here
 			}
 		}
-
-
+		name landingPage: "/"(controller: "user", action: "index")
+		
 		"/profile/$username?" (controller: "profile", action: "index")
 		"/profile/update" (controller: "profile", action: "update")
 		"/profile/updateSettings" (controller: "profile", action: "updateSettings")
 		"/profile/addFavorite" (controller: "profile", action: "addFavorite")
 		"/profile/removeFavorite" (controller: "profile", action: "removeFavorite")
 		"/profile/removeFavoriteFromSettings" (controller: "profile", action: "removeFavoriteFromSettings")
+		name settings: "/profile/settings"(controller:"profile", action: "settings")
+		name profileView: "/profile/$username" (controller:"profile", action:"index")
+		
 		name about: "/about"(controller:"base", action:"about")
 		name faqs: "/faqs"(controller:"base", action:"faqs")
+		
 		"/search"(controller:"search", action:"index")
+		
 		"/admin"(controller:"admin", action:"/index")
-		name settings: "/profile/settings"(controller:"profile", action: "settings")
-		name landingPage: "/"(controller: "user", action: "index")
-		name profileView: "/profile/$username" (controller:"profile", action:"index")
+		
 		"404"(controller: "error", action: "error404")
 	}
 }
